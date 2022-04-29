7a6333ab3051efb6b9a654f19704d260780e6ab6
Farid Shahidi <farid.sh69@gmail.com>
2022-04-29 18:48:50 +0430

    farid/routes: fix babel problem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import type { NextPage } from "next";
 import Head from "next/head";
 import { AppErrorBoundary } from "@components/AppErrorBoundary";
 import { RouteDetails } from "@components/RouteDetails";
 
-const Home = () => {
+const Home: NextPage = () => {
   return (
     <>
       <Head>
